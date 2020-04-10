cf95077406707d2546c081e1e6ca3dd0cd6da855
Viktor M <viktor.mikalsen@gmail.com>
2020-04-10 12:35:49 +0200

    Lagt inn euler, nice plottefunk og kjapp layout for implementering av RK4

---
+++ main.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot(t, plot, title):
+    """
+    Very useful plotting function, both useful for plot with one graph and multiple graphs
+    :param t: Array-like with time values for the plot
+    :param plot: Array-like with pairs of what is to be plotted along y-axis and label. i.g. [y, "position", z, "sinus"]
+    :param title: Title for the plot
+    :return: nothing, shows the graph at the end
+    """
+    plt.xlabel("Tid [$s$]")
+    plt.ylabel("Utslag")
+    plt.title(title)
+    for i in range(0, len(plot), 2):
+        plt.plot(t, plot[i], label=plot[i+1])
+    plt.legend(loc="best")
+    plt.show()
+
+def euler(y, f, h=h):
+    """
+    :param y: Vector with initial values for theta and chi
+    :param f: Stepfunction used for the nummerical approximation
+    :param h: Steplenght
+    """
+    n = int(N/h)
+    theta, chi = np.zeros(n), np.zeros(n)
+    theta[0] = y[0]
+    chi[0] = y[1]
+    xi = np.linspace(0,N,n)
+    for i in range(n-1):
+        theta[i+1] = theta[i] + h*f[0](theta[i], chi[i])
+        chi[i + 1] = chi[i] + h*f[1](theta[i], chi[i])
+    return xi, theta, chi
+
+def RK4_step():
+    "one step of Runge-Kutta"
+
+def RK4():
+    "Calls on RK4_step() to find the values for each iteration to solve the system of differential equations"
