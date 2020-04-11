b3d1d1cdd498670ff3b8aea407155573cd111d52
Viktor M <viktor.mikalsen@gmail.com>
2020-04-11 10:54:06 +0200

    sett på euler

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 
 def f(theta, chi, xi, n): return np.array([chi, -theta**n - 2*chi/xi])
 
+
 def plot(t, plot, title):
     """
     Very useful plotting function, both useful for plot with one graph and multiple graphs
@@ -11,30 +12,50 @@ def plot(t, plot, title):
     :param title: Title for the plot
     :return: nothing, shows the graph at the end
     """
-    plt.xlabel("Tid [$s$]")
-    plt.ylabel("Utslag")
+    plt.xlabel(r"$\xi$")
+    plt.ylabel(r"$\theta$")
     plt.title(title)
     for i in range(0, len(plot), 2):
         plt.plot(t, plot[i], label=plot[i+1])
     plt.legend(loc="best")
     plt.show()
 
-def euler(y, f, n, h=h):
+def euler(y, f, n, h):
     """
     :param y: Vector with initial values for theta and chi
     :param f: Stepfunction used for the nummerical approximation
+    :param n: Polytropic index
     :param h: Steplenght
     """
-    n = int(N/h)
-    theta, chi = np.zeros(n), np.zeros(n)
-    theta[0] = y[0]
-    chi[0] = y[1]
-    xi = np.linspace(0,N,n)
-    for i in range(n-1):
-        F = f(theta[i], chi[i], xi[i], n)
-        theta[i+1] = theta[i] + h*F[0]
-        chi[i + 1] = chi[i] + h*F[1]
+    theta = np.array([y[0]])
+    chi = np.array([y[1]])
+    xi = np.array([.001])
+    switch = True
+    while switch:
+        F = f(theta[-1], chi[-1], xi[-1], n)
+        theta = np.append(theta, theta[-1] + h*F[0])
+        chi = np.append(chi, chi[-1] + h*F[1])
+        xi = np.append(xi, xi[-1]+h)
+        #print("1")
+        if theta[-1]*theta[-2]<0:
+            switch = False
     return xi, theta, chi
+
+y = np.array([1,0])
+
+xi1, theta1, _ = euler(y, f, 1, .5)
+xi2, theta2, _ = euler(y, f, 1, .1)
+xi3, theta3, _ = euler(y, f, 1, .01)
+xi4, theta4, _ = euler(y, f, 1, .001)
+
+plt.plot(xi1, theta1, label ="h=.5")
+plt.plot(xi2, theta2, label="h=.1")
+plt.plot(xi3, theta3, label="h=.01")
+plt.plot(xi4, theta4, label="h=.001")
+plt.plot(xi4, np.sin(xi4)/xi4, label="analytisk")
+plt.legend(loc="best")
+plt.show()
+
 
 def RK4_step():
     "one step of Runge-Kutta"
