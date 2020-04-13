ffaf27ae5b4e11d243160e73397ff74bab0f017a
Viktor M <viktor.mikalsen@gmail.com>
2020-04-13 18:41:06 +0200

    Enda ikke rett for oppg. 3g. Kanskje i og j er ferdige?

--- main.py
+++ main.py
@@ -5,12 +5,25 @@ y = np.array([[1],[0]])
 xiN3 = 6.89684861937
 xiN3_2 = 3.653753736219229
 
+alpha = np.array([0.86, 0.59, 0.0167])
 
-def f(xi, y, n): return np.array([y[1], -np.abs(y[0])**n - 2*y[1]/xi])
+def f(xi, y, n):
+    """
+    Vector form of the Lane Embden equations. Modified with the correct value of \xi' for \xi = 0 (from piazza)
+    :param xi: Previous value of xi
+    :param y: Array with the previous values
+    :param n: Polytropic index
+    :return: Values used for the next values of y
+    """
+    if xi == 0:
+        return np.array([y[1], -1/3])
+    else:
+        return np.array([y[1], -np.abs(y[0])**n - 2*y[1]/xi])
 
 
 def euler(y, f, n, h):
     """
+    Numerically solves differential equations with the Euler-method
     :param y: Vector with initial values for theta and chi respectively
     :param f: Stepfunction used for the numerical approximation
     :param n: Polytropic index
@@ -18,7 +31,7 @@ def euler(y, f, n, h):
     :return: The numerical solution to the system of differential equations with the euler-method
     """
     Y = y
-    xi = np.array([.001])
+    xi = np.array([0])
     switch = True
     while switch:
         F = f(xi[-1], Y[:, -1], n)      #Y, last item of every array in Y
@@ -45,7 +58,7 @@ def RK4_step(xi, y, f, h, n):
     k2 = f(xi + h/2, y + h*k1/2, n)
     k3 = f(xi + h/2, y + h*k2/2, n)
     k4 = f(xi + h, y + h*k3, n)
-    S = 1/6*(k1 + 2*k2 + 2*k3 +k4)
+    S = 1/6*(k1 + 2*k2 + 2*k3 + k4)
     return y + h*S
 
 
@@ -59,7 +72,7 @@ def RK4_method(y, f, n, h):
     :return: The numerical solution to the system of differential equations with the RK4-method
     """
     Y = y
-    xi = np.array([.001])
+    xi = np.array([0])
     switch = True
     while switch:
         F = RK4_step(xi[-1], Y[:, -1], f, h, n)
@@ -71,7 +84,44 @@ def RK4_method(y, f, n, h):
     return xi[:-1],Y
 
 
-def error_funk(n0, n1, N, xi_N):
+def euler_error(y, f, n, h, N):
+    """
+    Numerically solves differential equations with the Euler-method
+    This function is ment for the error calculation, such that the last value of \xi is either xiN3_2 or xiN3
+    :param y: Vector with initial values for theta and chi respectively
+    :param f: Stepfunction used for the numerical approximation
+    :param n: Polytropic index
+    :param h: Steplenght
+    :return: The numerical solution to the system of differential equations with the euler-method
+    """
+    Y = y
+    xi = np.array([0])
+    for i in range(N+1):
+        F = f(xi[-1], Y[:, -1], n)      #Y, last item of every array in Y
+        placeholder = Y[:,-1] + h*F
+        Y = np.hstack((Y, [[item] for item in placeholder]))
+        xi = np.append(xi, xi[-1]+h)
+    return xi[:-1], Y
+
+def RK4_method_error(y, f, n, h, N):
+    """
+    Calls on RK4_step() to calculate the next values of theta and xi for each iteration
+    This function is ment for the error calculation, such that the last value of \xi is either xiN3_2 or xiN3
+    :param y: Vector with initial values for theta and chi respectively
+    :param f: Stepfunction used for the numerical approximation
+    :param n: Polytropic index
+    :param h: Steplenght
+    :return: The numerical solution to the system of differential equations with the RK4-method
+    """
+    Y = y
+    xi = np.array([0])
+    for i in range(N+1):
+        F = RK4_step(xi[-1], Y[:, -1], f, h, n)
+        Y = np.hstack((Y, [[item] for item in F]))
+        xi = np.append(xi, xi[-1]+h)
+    return xi[:-1],Y
+
+def error_funk(n0, n1, N, xi_N, n):
     """
     Function which evaluates the error
     :param n0: Starting-step
@@ -84,34 +134,43 @@ def error_funk(n0, n1, N, xi_N):
     RK4_err = np.array([1])
     xi1, xi2 = np.array(1), np.array(1)
     N_i = np.arange(n0, n1, N)
-    h_i = xi_N/N_i[::-1]
-    for h in h_i:
-        print(h)
-        theta1, xi1, _ = euler(y, f, 3, h)
-        theta2, xi2, _ = RK4_method(y, f, 3, h)
-        Euler_err = np.append(Euler_err, np.abs(theta1[-1]))
-        RK4_err = np.append(RK4_err, np.abs(theta2[-1]))
+    N_i = N_i[::-1]
+    h_i = xi_N/N_i
+    for i in range(len(h_i)):
+        xi1, Y1 = euler_error(y, f, n, h_i[i], N_i[i])
+        xi2, Y2 = RK4_method_error(y, f, n, h_i[i], N_i[i])
+        print(xi1[-1], xi2[-1])
+        Euler_err = np.append(Euler_err, np.abs(Y1[0][-1]))
+        RK4_err = np.append(RK4_err, np.abs(Y2[0][-1]))
     return Euler_err[1:], RK4_err[1:], h_i, xi1[-1], xi2[-1]
 
 def pfunc(x, p, alpha): return np.array(-0.5*alpha*x*(1+p)*(1+3*p)*(1-x**2*alpha)**-1)
+def analy_p(x, alpha): return ((np.sqrt(1-alpha)-np.sqrt(1-alpha*x**2))/(np.sqrt(1-alpha*x**2)-3*np.sqrt(1-alpha)))/ \
+                              ((np.sqrt(1-alpha)-1)/(1-3*np.sqrt(1-alpha)))
 
-def solv_p(alph, pfunc, h):
+def P(alpha, h, switch=False):
     """
-
-    :param alph: liste med 3 alpha verdier, verdt å huske at vi da må iterere gjennom den og
-    :param pfunc: funksjon for deriverte av p, er likn 36
-    :param h: trinnlengde, gikk for 0.01 for simpelhetskyld
-    :return: et 3x2xN rom, hvor N er lengden på skritt som blir tatt av euler eller rk4. Mulig problem oppstår om de har ulik antall total skritt
+    Solves the TOV-equations numerically, and compares with the analytical result
+    :param alpha: Array-like with the values of alpha we want to plot for
+    :param h: Stepsize
+    :return: Nothing, plots the graphs
     """
-    solved_p = np.array([[1,1],[2,2],[3,3]])
-    for i in range(3):
-        p0 = np.array([[(np.sqrt (1 - alph[i]) - 1) / (1 - 3 * np.sqrt (1 - alph[i]))]])
-        solved_p[i][0] = euler(p0, pfunc, alph[i], h)
-        solved_p[i][1] = RK4_method(p0, pfunc, alph[i], h)
-    return solved_p
-
-def analy_p( x, alph): return (((np.sqrt(1-alph)-np.sqrt(1-alph*x**2))/(np.sqrt(1-alph*x**2)-3*np.sqrt(1-alph))))/ \
-                              ((np.sqrt(1-alph)-1)/(1-3*np.sqrt(1-alph)))
+    for i in range(len(alpha)):
+        p0 = np.array([[(np.sqrt(1 - alpha[i]) - 1) / (1 - 3 * np.sqrt(1 - alpha[i]))]])
+        xi1, Y1 = euler(p0, pfunc, alpha[i], h)
+        xi2, Y2 = RK4_method(p0, pfunc, alpha[i], h)
+        plt.figure()
+        plt.plot(xi1, Y1[0], label="Euler")
+        plt.plot(xi2, Y2[0],'-.', label="RK4")
+        plt.plot(xi1, analy_p(xi1, alpha[i]), label="Analyisk")
+        if switch:
+            plt.plot(xi1, 0.25*alpha[i]*(1-xi1**2), '-.', label="Newtonian approximation")
+        plt.legend(loc='best')
+        plt.xlabel("x-axis")
+        plt.ylabel("P-axis")
+        tittel = "h = " + str(h) + ", alpha = " + str(alpha[i])
+        plt.title(tittel)
+        plt.show()
 
 
 def plot3_d_f(method, title, n=1, y=y, f=f):
@@ -139,9 +198,23 @@ def plot3_d_f(method, title, n=1, y=y, f=f):
     plt.xlabel(r"$\xi$")
     plt.ylabel(r"$\theta(\xi)$")
     plt.show()
-
-
-def plot1_3g(n0, n1, N):
+def plt3_e(h):
+    """
+    Solves Lane-Emden equations numerically
+    :param h: Stepsize
+    :return: Nothing, plots the graphs
+    """
+    xi1, Y1 = euler(y, f, 3/2, h)
+    xi2, Y2 = euler(y, f, 3, h)
+    plt.figure()
+    plt.title("Nummerisk løsning for n=3/2 og n =3")
+    plt.plot(xi1, Y1[0], '-.', label="Ikke-relativistiske tilfellet (n=3/2)")
+    plt.plot(xi2, Y2[0], '-.', label="Ultrarelativistiske tilfellet (n=3)")
+    plt.legend(loc='best')
+    plt.xlabel(r"$\xi$")
+    plt.ylabel(r"$\theta(\xi)$")
+    plt.show()
+def plot3_1g(n0, n1, N):
     """
     Plot-function for the ultra-relativistic case in task 3g
     :param n0: Starting-step
@@ -149,7 +222,7 @@ def plot1_3g(n0, n1, N):
     :param N: Stepsice
     :return: Nothing, plots the graph
     """
-    Euler_err, RK4_err, h_i, xi1, xi2 = error_funk(n0, n1, N, xiN3)
+    Euler_err, RK4_err, h_i, xi1, xi2 = error_funk(n0, n1, N, xiN3, 3)
     plt.figure()
     plt.plot(h_i, Euler_err, '-.', label="Ultrarelativistisk euler error")
     plt.plot(h_i, RK4_err, '-.', label="Ultrarelativistisk RK4 error")
@@ -159,9 +232,7 @@ def plot1_3g(n0, n1, N):
     plt.ylabel("Theta error")
     plt.xlabel("Trinnlengde h")
     plt.show()
-
-
-def plot2_3g(n0, n1, N):
+def plot3_2g(n0, n1, N):
     """
     Plot-function for the non-relativistic case in task 3g
     :param n0: Starting-step
@@ -169,7 +240,7 @@ def plot2_3g(n0, n1, N):
     :param N: Stepsize
     :return: Nothing, plots the graph
     """
-    Euler_err, RK4_err, h_i, xi1, xi2 = error_funk(n0, n1, N, xiN3_2)
+    Euler_err, RK4_err, h_i, xi1, xi2 = error_funk(n0, n1, N, xiN3_2, 3/2)
     plt.figure()
     plt.plot(h_i, Euler_err, '-.', label="Ikke-relativistisk euler error")
     plt.plot(h_i, RK4_err, '-.', label="Ikke-relativistisk RK4 error")
@@ -180,40 +251,24 @@ def plot2_3g(n0, n1, N):
     plt.xlabel("Trinnlengde h")
     plt.show()
 
-def plot3_i(alph, solved_p, analy_p, h, a ):
-    """
 
-    :param alph: liste med alpha verdier
-    :param solved_p: rom som inneholder løsninger for p
-    :param analy_p: normalisert analytisk løsning for newton redusert approximasjon
-    :param h: skritt
-    :param a: boolk statement som bestemmer plot av analytisk
-    :return: nada
-    """
-    for i in range(len(alph)):
-        x = np.linspace(0, 1, 1000)
-        plt.figure()
-        plt.title("sammenlikning euler, rk4 og analytisk for h=", h, "og alpha = ", alph[i])
-        plt.plot(x, analy_p(x, alph[i]), 'b.', label="analytisk")
-        plt.plot(x, solved_p[i][0], 'r.', label="euler")
-        plt.plot(x, solved_p[i][1], 'g.', label="RK 4")
-        if a:
-            plt.plot(x, (0.25*alph*(1-x**2)), '.', label="newtonsk approximasjon")
-        plt.legend()
-        plt.xscale("log")
-        plt.yscale("log")
-        plt.show()
+
 
 #Plotter oppgave 3d
-plot3_d_f(euler, "Numerical solution with the Euler-method")
+#plot3_d_f(euler, "Numerical solution with the Euler-method, n=1")
+
+#Plotter oppgave 3e
+#plt3_e(0.0001)
 
 #Plotter oppgave 3f
-#plot3_d_f(RK4_method, "Numerical solution with the RK4-method")
+#plot3_d_f(RK4_method, "Numerical solution with the RK4-method, n=1")
 
-#plot1_3g(1, 1000, 1)
+#Plotter oppgave 3g
+#plot3_1g(10, 100, 1)
+#plot3_2g(10, 100, 1)
 
-# oppg 3i
+#Plotter oppgave 3i
+#P(alpha, 0.001)
 
-alp =[0.86, 0.59, 0.0167]
-solved_p = solv_p(alp, pfunc, 0.01)
-plot3_i(alp, solved_p, analy_p, 0.01, False)
+#Plotter oppgave 3j
+#P(alpha, 0.001, True)
